130f2d70ba586ad3aeafb2b1900a82fa84756753
unknown <fiacregiraneza@gmail.com>
2022-01-29 08:53:57 +0200

    backend

--- index.js
+++ index.js
@@ -12,14 +12,38 @@ const conversationRoute = require("./routes/conversations");
 const messageRoute = require("./routes/messages")
 const router = express.Router();
 const path = require("path");
+const cors = require("cors");
+const bodyParser = require('body-parser');
 
 dotenv.config();
 
 mongodb();
-app.use("/images", express.static(path.join(__dirname, "public/images")));
+app.use(cors());
 
 //middleware
 app.use(express.json());
+app.use("/images", express.static(path.join(__dirname, "public/images")));
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
+app.use("/api/messages", messageRoute);
+app.use("/api/conversations", conversationRoute);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.get('/', (req, res) => res.status(200).json({ message: 'Welcome to my api' }));
+app.use((req, res, next) => { res.status(400).json({ Error: 'Invalid Request' }); next();});
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  // res.setHeader('Origin', '*');
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'Authorization ,content-type');
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  // Pass to next layer of middleware
+  next();
+});
 app.use(helmet());
 app.use(morgan("common"));
 
@@ -41,12 +65,6 @@ app.post("/api/upload", upload.single("file"), (req, res) => {
   }
 });
 
-app.use("/api/auth", authRoute);
-app.use("/api/users", userRoute);
-app.use("/api/posts", postRoute);
-app.use("/api/messages", messageRoute);
-app.use("/api/conversations", conversationRoute);
-
-app.listen(8800, () => {
-  console.log("Backend server is running!");
-});
+app.listen(process.env.PORT || 8800, 
+  console.log("Backend server is running!")
+);
